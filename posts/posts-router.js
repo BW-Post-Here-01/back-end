8db62299f0bda1884c16e7e752460c91dcb6d179
j-greenberg <jgreenberg@runbox.com>
2020-04-27 18:04:58 -0400

    added CRUD operations to Posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,6 +2,20 @@ const router = require("express").Router();
 
 const Posts = require("./posts-model.js"); 
 
+router.post('/', (req, res) => {
+    let post = req.body; 
+
+    Posts.add(post)
+        .then(response => {
+            console.log(response); 
+            res.status(201).json({ message: response }); 
+        })
+        .catch(error => {
+            console.log(error); 
+            res.status(500).json({ errorMessage: error.message }); 
+        })
+}); 
+
 router.get('/', (req, res) => {
     Posts.find()
         .then(posts => {
@@ -13,5 +27,34 @@ router.get('/', (req, res) => {
             res.status(500).json({ errorMessage: error.message }); 
         })
 })
+
+router.put('/:id', (req, res) => {
+    let id = req.params.id; 
+    let post = req.body; 
+
+    Posts.update(id, post)
+        .then(response => {
+            console.log(response); 
+            res.status(200).json({ response }); 
+        })
+        .catch(error => {
+            console.log(error); 
+            res.status(500).json({ errorMessage: error.message }); 
+        })
+}); 
+
+router.delete('/:id', (req, res) => {
+    let id = req.params.id; 
+
+    Posts.remove(id)
+        .then(response => {
+            console.log(response); 
+            res.status(200).json({ message: `Post Id#${id} was successfully deleted!` }); 
+        })
+        .catch(error => {
+            console.log(error); 
+            res.status(500).json({ errorMessage: error.message }); 
+        }); 
+}); 
 
 module.exports = router; 
