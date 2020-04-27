236a945300c72370d6f0477869db538e45b2aaa7
j-greenberg <jgreenberg@runbox.com>
2020-04-27 00:30:44 -0400

    added nodemon

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express"); 
 const server = express(); 
 const cors = require("cors"); 
+const helmet = require("helmet"); 
 
 server.use(express.json()); 
 server.use(cors()); 
--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require("./api/server.js");
 const PORT = process.env.PORT || 5000; 
 
 server.listen(PORT, () => {
-    console.log(`Server listening on ${port}`); 
+    console.log(`Server listening on ${PORT}`); 
 })
