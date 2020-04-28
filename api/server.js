213440c36d9e9ad3daa7e4c5fd6f903b0d521acd
j-greenberg <jgreenberg@runbox.com>
2020-04-28 12:46:58 -0400

    streamlined endpoints

--- api/server.js
+++ api/server.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const helmet = require("helmet"); 
 
 const authRouter = require("../auth/auth-router.js"); 
-const postsRouter = require("../posts/posts-router.js"); 
 const usersRouter = require('../users/users-router.js'); 
 const authenticator = require("../auth/authenticator.js"); 
 
@@ -14,7 +13,6 @@ server.use(cors());
 server.use(helmet()); 
 
 server.use('/api/auth', authRouter); 
-server.use('/api/posts', authenticator, postsRouter); 
 server.use('/api/users', authenticator, usersRouter); 
 
 server.get("/", (req, res) => {
--- users/users-router.js
+++ users/users-router.js
@@ -3,6 +3,20 @@ const router = express.Router();
 
 const Users = require('./users-model.js'); 
 
+router.get('/:id', (req, res) => {
+    let id = req.params.id; 
+    
+    Users.findById(id)
+        .then(response => {
+            console.log(response); 
+            res.status(200).json({ response }); 
+        })
+        .catch(error => {
+            console.log(error); 
+            res.status(500).json({ errorMessage: error.message }); 
+        })
+})
+
 router.put('/:id', (req, res) => {
     let user = req.body; 
     let id = req.params.id; 
@@ -31,5 +45,65 @@ router.delete('/:id', (req, res) => {
             res.status(500).json({ errorMessage: error.message }); 
         })
 })
+
+// POSTS
+
+const Posts = require("./posts-model.js"); 
+
+router.post('/:id/posts', (req, res) => {
+    let post = req.body; 
+    let id = req.params.id; 
+
+    Posts.add(post)
+        .then(response => {
+            console.log(response); 
+            res.status(201).json({ message: response }); 
+        })
+        .catch(error => {
+            console.log(error); 
+            res.status(500).json({ errorMessage: error.message }); 
+        })
+}); 
+
+router.get('/', (req, res) => {
+    Posts.find()
+        .then(posts => {
+            console.log(posts); 
+            res.status(200).json({ posts }); 
+        })
+        .catch(error => {
+            console.log(error); 
+            res.status(500).json({ errorMessage: error.message }); 
+        })
+})
+
+router.put('/:id', (req, res) => {
+    let id = req.params.id; 
+    let post = req.body; 
+
+    Posts.update(id, post)
+        .then(response => {
+            console.log(response); 
+            res.status(200).json({ response }); 
+        })
+        .catch(error => {
+            console.log(error); 
+            res.status(500).json({ errorMessage: error.message }); 
+        })
+}); 
+
+router.delete('/:id', (req, res) => {
+    let id = req.params.id; 
+
+    Posts.remove(id)
+        .then(response => {
+            console.log(response); 
+            res.status(200).json({ message: `Post Id#${id} was successfully deleted!` }); 
+        })
+        .catch(error => {
+            console.log(error); 
+            res.status(500).json({ errorMessage: error.message }); 
+        }); 
+}); 
 
 module.exports = router; 
